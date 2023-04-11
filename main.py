45f6240ea88345055c6ca1ba4e655118fe36d518
realfaizanahmed <112700497+realfaizanahmed@users.noreply.github.com>
2023-04-11 23:28:43 +0500

    Connected to database and improved some frontend part

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/codingthunder"
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 
 @app.route("/")
@@ -12,8 +26,17 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET','POST'])
 def contact():
+    if(request.method=='POST'):
+        #ADD ENTRY TO DATABASE
+        name = request.form.get('name')
+        email= request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num = phone, msg = message, date = datetime.now(),email = email)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 @app.route("/post")
